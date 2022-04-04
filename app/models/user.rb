9c0e1a39c9ed7348e6c2102873a9912085dae65e
YasugiMomo <yasugimomo.crw@gmail.com>
2022-04-04 07:19:28 +0000

    [Update]応用課題4

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,34 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  # foreign_key（FK）には、@user.xxxとした際に「@user.idがfollower_idなのかfollowed_idなのか」を指定します。
+  # follower
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # followed
+  has_many :reverse_of_relationshiops, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # @user.booksのように、@user.yyyで、
+  # 一覧を表示 through スルーするテーブル source 参照するカラム
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :relationships, source: :follower
+  
 
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50}
-
+  
+  # フォローしたときの処理
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  # フォローを外したときの処理 
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  # フォローしているか判定
+  def following?(user)
+    followings.include?(user)
+    
 
 
   def get_profile_image
