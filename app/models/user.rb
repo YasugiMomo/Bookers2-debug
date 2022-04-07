864478ea28fdb564dd9c4d447b2fb4631c511ec9
YasugiMomo <yasugimomo.crw@gmail.com>
2022-04-07 03:32:04 +0000

    [Update]応用課題5 viewの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,7 +35,22 @@ class User < ApplicationRecord
   def following?(user)
     followings.include?(user)
   end
-    
+  
+  # 検索機能の定義 search　メソッド
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+  end
+      
 
 
   def get_profile_image
